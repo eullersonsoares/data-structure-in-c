946825747c4d3e941790a303cfbb7575b4c3ab7b
Renata Leal <renataleal@Renatas-MacBook-Pro.local>
2025-02-04 21:18:26 -0300

    automatic memory allocation

---
+++ automatic-memory-allocation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void functionExample() {
+    int number = 10; //automatic variable;
+    printf("Number value: %d\n", number);
+}
+
+int main() {
+    functionExample();
+    return 0;
+}
